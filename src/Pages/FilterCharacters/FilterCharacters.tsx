21f3fa81635dff5c50512e8e1ec7722342c93c4f
tamsay <temitope.longe@decagon.dev>
2021-08-29 19:03:27 +0100

    warning errors removed

--- src/Pages/FilterCharacters/FilterCharacters.tsx
+++ src/Pages/FilterCharacters/FilterCharacters.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {useState, useEffect} from 'react';
 import styles from './FilterCharactersStyles.module.scss';
 import cx from 'classnames';
